51ddfcb32f5900d5ac571f92c51da39d0bb779b8
ogbeche77 <ogbeche77@yahoo.com>
2020-05-04 13:44:37 +0200

    style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
 import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css';
 export class App extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                <Cards/>
                <CountryPicker/>
                <Chart/>
