ee4e772a70ccfe2fd5b233fd20a3b6becb15fa54
ogbeche77 <ogbeche77@yahoo.com>
2020-05-04 13:36:14 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import {Cards, Chart, CountryPicker} from './components'
 export class App extends Component {
     render() {
         return (
             <div>
-                <h1> Covid</h1>
+               <Cards/>
+               <CountryPicker/>
+               <Chart/>
             </div>
         )
     }
