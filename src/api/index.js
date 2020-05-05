6a72700e04436d477e04f611f3de4c991b0df7c6
ogbeche77 <ogbeche77@yahoo.com>
2020-05-05 15:11:23 +0200

    countries api

--- src/api/index.js
+++ src/api/index.js
@@ -32,10 +32,11 @@ try {
 
 
 //Countries
-export const countries = async () => {
+export const fetchCountries = async () => {
     try {
-        const response = await axios.get(`${url}/countries`);
-        console.log(response);
+        const {data: { countries }} = await axios.get(`${url}/countries`);
+        return countries.map((country)=> country.name);
+
     } catch (error) {
         console.log(error);
     }
