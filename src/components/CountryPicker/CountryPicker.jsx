cab938d58e7e04ff54d2ceac946bc9f939bf2988
ogbeche77 <ogbeche77@yahoo.com>
2020-05-07 14:22:26 +0200

    country picker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -3,7 +3,7 @@ import { NativeSelect, FormControl } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import {fetchCountries } from '../../api';
 
-const CountryPicker = ({handleCountryChange}) => {
+const CountryPicker = ({ handleCountryChange }) => {
     const [fetchedCountries, setFetchedCountries] = useState([]);
 
     useEffect(() => {
