9e9c0d9058ed6d0d186cdb1b0cb1674e7b6edc11
ogbeche77 <ogbeche77@yahoo.com>
2020-05-07 14:22:47 +0200

    country url

--- src/api/index.js
+++ src/api/index.js
@@ -5,16 +5,18 @@ const url = 'https://covid19.mathdro.id/api';
 //Cards
 export const fetchData = async (country) => {
     let changeableUrl = url;
+
     if(country) {
-        changeableUrl = `{url}/countries/${country}`
+        changeableUrl = `${url}/countries/${country}`
     }
-    
+
 
     try {
-        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
         
          return {confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
+        console.log(error)
 
     }
 }
