7de0f6141c294338e41934aece75b1472268e0e9
ogbeche77 <ogbeche77@yahoo.com>
2020-05-08 14:03:36 +0200

    image added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Cards, Chart, CountryPicker} from './components'
 import styles from './App.module.css';
 import { fetchData } from './api';
+import image from './images/image.png';
 export class App extends Component {
     state = {
         data: {},
@@ -30,6 +31,7 @@ export class App extends Component {
         
         return (
             <div className={styles.container}>
+            <img className={styles.image} src={image} alt="corona"/>
                <Cards data={data}/>
                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                <Chart data= {data} country={country}/>
