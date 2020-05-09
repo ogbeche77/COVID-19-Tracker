fd46c0b120b13651be580c2f10c538d905ac2860
ogbeche77 <ogbeche77@yahoo.com>
2020-05-09 13:47:00 +0200

    Updates

---
+++ precache-manifest.49e95bcdf7d6abd10718f8265c29ae32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54fc6907363b51745b6e906219436a0",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "e914482a0a5378e05d77",
+    "url": "/COVID-19-Tracker/static/css/main.8baea6cd.chunk.css"
+  },
+  {
+    "revision": "edd4220ecfd16097c5be",
+    "url": "/COVID-19-Tracker/static/js/2.742d373e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.742d373e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e914482a0a5378e05d77",
+    "url": "/COVID-19-Tracker/static/js/main.1f8fa8d0.chunk.js"
+  },
+  {
+    "revision": "7774cd54833ddc4c701e",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.4fea0175.js"
+  },
+  {
+    "revision": "2289c33e36c1f803a9f2585a34bd4dee",
+    "url": "/COVID-19-Tracker/static/media/image1.2289c33e.jpg"
+  }
+]);
