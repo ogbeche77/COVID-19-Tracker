8976ea09217a7c5671faaa5f38cd8e1d20b5d460
ogbeche77 <ogbeche77@yahoo.com>
2020-05-10 14:24:46 +0200

    comments

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NativeSelect, FormControl } from '@material-ui/core';
+import { NativeSelect, FormControl } from '@material-ui/core'; //used for creating country picker
 import styles from './CountryPicker.module.css';
 import {fetchCountries } from '../../api';
 
