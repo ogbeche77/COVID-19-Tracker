19a450eed45c7ae9f757f5a8a1deddd076a196ad
ogbeche77 <ogbeche77@yahoo.com>
2020-05-10 14:06:29 +0200

    comments

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+//efficient way of importing to app.js
 export { default as Cards} from './Cards/Cards';
 export { default as Chart} from './Chart/Chart';
 export { default as CountryPicker} from './CountryPicker/CountryPicker';
