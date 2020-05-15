6b119693734251ac93b19d23eb71c1a76dbf96ae
ogbeche77 <ogbeche77@yahoo.com>
2020-05-15 13:40:58 +0200

    comments

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,7 @@ export const fetchData = async (country) => {
 //Api function for Charts.jsx
 export const fetchDailyData = async () =>{
 try {
-    const { data } = await axios.get(`${url}/daily`); // fetching dailt data
+    const { data } = await axios.get(`${url}/daily`); // fetching daily data
 
     const modifiedData = data.map((dailyData)=> ({
         confirmed: dailyData.confirmed.total,
