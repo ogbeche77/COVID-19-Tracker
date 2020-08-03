a8e2a9c7014eaead5bc09d5b9e900ca9a0726bdf
ogbeche77 <ogbeche77@yahoo.com>
2020-08-03 13:04:00 +0200

    Make code pretty

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,65 +1,65 @@
-import React, {useState, useEffect} from 'react'; //Effect Hooks enables us to create side effects without creating a CBC
+import React, { useState, useEffect } from 'react'; //Effect Hooks enables us to create side effects without creating a CBC
 import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2'; //both chart.js & react-chartjs2 is need as dependencies
 import styles from './Chart.module.css';
 
-const Chart = ({ data: {confirmed, recovered, deaths}, country }) => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [dailyData, setDailyData] = useState({});
 
     useEffect(() => {
         const fetchAPI = async () => {
             const initialDailyData = await fetchDailyData();
             setDailyData(initialDailyData);
-        } 
+        }
 
         fetchAPI();
     }, []); //2nd argument, the empty bracket ensures useEffect is called only when component mounts
 
     const lineChart = (
         dailyData.length ? (
-        <Line
-        data={{
-        labels:dailyData.map(({date})=> date),
-        datasets: [{
-            data: dailyData.map(({confirmed})=> confirmed),
-            label: "Infected",
-            borderColor: "rgba(111, 166, 232)",
-            fill: true,
-        }, 
-        {
-            data: dailyData.map(({deaths})=> deaths),
-            label: "Deaths",
-            borderColor: "rgba(97, 21, 29)",
-            fill: true,
-        }],
-    }}
-        />) : null 
+            <Line
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [{
+                        data: dailyData.map(({ confirmed }) => confirmed),
+                        label: "Infected",
+                        borderColor: "rgba(111, 166, 232)",
+                        fill: true,
+                    },
+                    {
+                        data: dailyData.map(({ deaths }) => deaths),
+                        label: "Deaths",
+                        borderColor: "rgba(97, 21, 29)",
+                        fill: true,
+                    }],
+                }}
+            />) : null
     );
-        //labels, dataset etc  synthax from chart.js documentation
-        const barChart = (
+    //labels, dataset etc  synthax from chart.js documentation
+    const barChart = (
         confirmed ? (
             <Bar
-            data={{
-                labels: ["Infected", "Recovered", "Deaths"],
-                datasets: [{
-                    label:"People",
-                    backgroundColor: [ "rgba(111, 166, 232)", "rgba(133, 242, 131)", "rgba(97, 21, 29)"],
-                    data : [confirmed.value, recovered.value, deaths.value]
-                }]
+                data={{
+                    labels: ["Infected", "Recovered", "Deaths"],
+                    datasets: [{
+                        label: "People",
+                        backgroundColor: ["rgba(111, 166, 232)", "rgba(133, 242, 131)", "rgba(97, 21, 29)"],
+                        data: [confirmed.value, recovered.value, deaths.value]
+                    }]
 
 
-            }}
-            options ={{
-                legend: {display: false},
-                title: {display: true, text: `Current state in ${country}`}
+                }}
+                options={{
+                    legend: { display: false },
+                    title: { display: true, text: `Current state in ${country}` }
 
-            }}
-             />
+                }}
+            />
         ) : null
     )
     return (
         <div className={styles.container}>
-        {country ? barChart : lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
