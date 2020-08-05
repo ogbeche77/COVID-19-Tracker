1cc10970f0070db4bfcef5835f8e4c20c7cca1c8
ogbeche77 <ogbeche77@yahoo.com>
2020-08-05 10:27:36 +0200

    Made code pretty

--- src/api/index.js
+++ src/api/index.js
@@ -7,44 +7,46 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async (country) => {
     let changeableUrl = url;
 
-    if(country) {
+    if (country) {
         changeableUrl = `${url}/countries/${country}`
     }
 
 
     try {
-        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
-        
-         return {confirmed, recovered, deaths, lastUpdate };
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+        return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
         console.log(error)
 
     }
 }
 
+
 //Api function for Charts.jsx
-export const fetchDailyData = async () =>{
-try {
-    const { data } = await axios.get(`${url}/daily`); // fetching daily data
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`); // fetching daily data
 
-    const modifiedData = data.map((dailyData)=> ({
-        confirmed: dailyData.confirmed.total,
-        deaths: dailyData.deaths.total,
-        date: dailyData.reportDate,
-    }));
-    return modifiedData;
-} catch (error) {
-    console.log(error)
 
-}
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+        return modifiedData;
+    } catch (error) {
+        console.log(error)
+
+    }
 }
 
 
 //Api function CountryPicker.jsx
 export const fetchCountries = async () => {
     try {
-        const {data: { countries }} = await axios.get(`${url}/countries`);
-        return countries.map((country)=> country.name);
+        const { data: { countries } } = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name);
 
     } catch (error) {
         console.log(error);
