e9e5dd2601da626d653490930ec212b95378943b
ogbeche77 <ogbeche77@yahoo.com>
2020-08-29 15:18:20 +0200

    Footer component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css';
 import { fetchData } from './api'; //To call function from index/api.js
 import image from './images/image1.jpg';
+import Footers from './components/Footers/Footers';
 
 
 export class App extends Component {
@@ -38,7 +39,10 @@ export class App extends Component {
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart data={data} country={country} />
+                <Footers />
             </div>
+
+
         )
     }
 }
