35ef5c93adf251da3f43d89475363282f305997d
ogbeche77 <ogbeche77@yahoo.com>
2020-10-18 12:16:49 +0200

    Code refactored

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -4,23 +4,23 @@ import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
 const CountryPicker = ({ handleCountryChange }) => {
-    const [fetchedCountries, setFetchedCountries] = useState([]);
+    const [countries, setCountries] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
-            setFetchedCountries(await fetchCountries());
+            setCountries(await fetchCountries());
 
         }
 
         fetchAPI();
-    }, [setFetchedCountries]);
+    }, []);
 
 
     return (
         <FormControl className={styles.formControl}>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
                 <option value="">Global </option>
-                {fetchedCountries.map((country, i) => <option key={i} value={country}> {country} </option>)}
+                {countries.map((country, i) => <option key={i} value={country}> {country} </option>)}
             </NativeSelect>
 
         </FormControl>
