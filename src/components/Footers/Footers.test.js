3b7d95cddc191ac3762bf0a4132e337888fe3a49
ogbeche77 <ogbeche77@yahoo.com>
2021-06-21 12:30:31 +0200

    Added unit test

---
+++ src/components/Footers/Footers.test.js
@@ -0,0 +1,9 @@
+import { shallow } from "enzyme";
+import toJSON from "enzyme-to-json";
+import React from "react";
+import Footers from "./Footers";
+
+test("matches snapshot", () => {
+  const wrapper = shallow(<Footers />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
