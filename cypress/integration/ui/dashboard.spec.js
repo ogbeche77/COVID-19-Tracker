357a8fa7bc5c45bea3e4fd83f65ab9b40351ca1c
ogbeche77 <ogbeche77@yahoo.com>
2021-06-26 12:08:24 +0200

    Fixed failing cypress test

--- cypress/integration/ui/dashboard.spec.js
+++ cypress/integration/ui/dashboard.spec.js
@@ -4,16 +4,12 @@ import {
   cyDeathDashBoard,
 } from "../../../src/handles/index";
 
-describe("End ChatPlugin Conversation", () => {
+describe("Confirm that total number of COVID infection", () => {
   it("Confirm that total number of COVID infection, Recovery & Death are present", () => {
     cy.visit("http://localhost:3000/COVID-19-Tracker");
 
-    cy.get(`[data-cy=${cyInfectedDashBoard}]`)
-      .children()
-      .should("have.length", 1);
-    cy.get(`[data-cy=${cyRecoveredDashBoard}]`)
-      .children()
-      .should("have.length", 1);
-    cy.get(`[data-cy=${cyDeathDashBoard}]`).children().should("have.length", 1);
+    cy.get(`[data-cy=${cyInfectedDashBoard}]`).should("have.length", 1);
+    cy.get(`[data-cy=${cyRecoveredDashBoard}]`).should("have.length", 1);
+    cy.get(`[data-cy=${cyDeathDashBoard}]`).should("have.length", 1);
   });
 });
